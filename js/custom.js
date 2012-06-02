79bf112f7a6fda4ef7389e39f3d15f9013c53725
Richard Bray <richardobray@yahoo.co.uk>
2012-06-02 23:55:41 +0100

    nearly finished

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 
 
+
 // Credits: Robert Penners easing equations (http://www.robertpenner.com/easing/).
 jQuery.easing['BounceEaseOut'] = function(p, t, b, c, d) {
 	if ((t/=d) < (1/2.75)) {
@@ -24,6 +25,7 @@ jQuery.easing['BounceEaseOut'] = function(p, t, b, c, d) {
 	};
 	
 
+
 jQuery(document).ready(function() {
     jQuery('#mycarousel').jcarousel({
 		scroll: 1,
@@ -35,14 +37,38 @@ jQuery(document).ready(function() {
 		$.scrollTo( '.work', 1200, {easing:'elasout'} );
 	});
 	
-	$('.top').click(function(){
-		$.scrollTo( '.head', 1200, {easing:'elasout'} );
-	});
-	
 	$(".more").click(function(){
 		$(".more-work").slideToggle({easing:'BounceEaseOut'});
 		$('.more').toggle();
 	});
+	
+	$("#up").click(function(){
+		$.scrollTo( '.about', 1200, {offset:-500} );
+	});
+	
+	$("#down").click(function(){
+		$.scrollTo( '.work', 1200, {} );
+	});
+	
+	$(".fancybox")
+		.attr('rel', 'gallery')
+		.fancybox({
+			openMethod : 'dropIn',
+			openSpeed : 250,
+
+			closeMethod : 'dropOut',
+			closeSpeed : 150,
+			
+			nextMethod : 'slideIn',
+			nextSpeed : 250,
+			
+			prevMethod : 'slideOut',
+			prevSpeed : 250
+		});
+		
+	$('[rel=tooltip]').tooltip();
+	
+	$('input[placeholder], textarea[placeholder]').placeholder();
 
 });
 
@@ -64,12 +90,70 @@ jQuery(document).ready(function() {
 graphByMonth = new Array(
 		[90,'Photoshop'],
 		[80,'HTML/CSS'],
-		[20,'Rails'],
-		[50,'Jquery'],
+		[50,'jQuery'],
 		[20,'SCSS'],
-		[25,'Git cmds'],
+		[25,'Git'],
 		[90,'3DS Max'],
 		[60,'Mubdox']		
 	);
 
 
+	(function ($, F) {
+    
+    // Opening animation - fly from the top
+    F.transitions.dropIn = function() {
+        var endPos = F._getPosition(true);
+
+        endPos.top = (parseInt(endPos.top, 10) - 200) + 'px';
+        endPos.opacity = 0;
+        
+        F.wrap.css(endPos).show().animate({
+            top: '+=200px',
+            opacity: 1
+        }, {
+            duration: F.current.openSpeed,
+            complete: F._afterZoomIn
+        });
+    };
+
+    // Closing animation - fly to the top
+    F.transitions.dropOut = function() {
+        F.wrap.removeClass('fancybox-opened').animate({
+            top: '-=200px',
+            opacity: 0
+        }, {
+            duration: F.current.closeSpeed,
+            complete: F._afterZoomOut
+        });
+    };
+    
+    // Next gallery item - fly from left side to the center
+    F.transitions.slideIn = function() {
+        var endPos = F._getPosition(true);
+
+        endPos.left = (parseInt(endPos.left, 10) - 200) + 'px';
+        endPos.opacity = 0;
+        
+        F.wrap.css(endPos).show().animate({
+            left: '+=200px',
+            opacity: 1
+        }, {
+            duration: F.current.nextSpeed,
+            complete: F._afterZoomIn
+        });
+    };
+    
+    // Current gallery item - fly from center to the right
+    F.transitions.slideOut = function() {
+        F.wrap.removeClass('fancybox-opened').animate({
+            left: '+=200px',
+            opacity: 0
+        }, {
+            duration: F.current.prevSpeed,
+            complete: function () {
+                $(this).trigger('onReset').remove();
+            }
+        });
+    };
+	
+	}(jQuery, jQuery.fancybox));
